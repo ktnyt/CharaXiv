6bc15d11db0f03e66e27e1de15992626bd5c87ab
ktnyt <mirei.itaya@gmail.com>
2023-11-23 16:00:32 +0900

    [from now] 2023/11/23 16:00:32
    diff --git a/frontend/src/components/Picker.tsx b/frontend/src/components/Picker.tsx
    index 2764416..b6ae17f 100644
    --- a/frontend/src/components/Picker.tsx
    +++ b/frontend/src/components/Picker.tsx
    @@ -1,20 +1,33 @@
     import clsx from "clsx";
    -import { Component, createSignal } from "solid-js";
    +import { Component, For, createSignal } from "solid-js";
    +import { sequence } from "./utils";
    +import { Stack } from "./Stack";
    
     export type PickerProps = {};
    
     export const Picker: Component<PickerProps> = (props) => {
       const [active, setActive] = createSignal(false);
    +
    +  const tensIndex = 0;
    +  const onesIndex = 0;
    +
       return (
    -    <div>
    -      <div
    -        class={clsx(
    -          "flex items-center justify-center bg-blue-50 transition-all",
    -          active() ? "h-16 w-16" : "h-8 w-8",
    -        )}
    -        onClick={() => setActive((prev) => !prev)}
    -      >
    -        <div>42</div>
    +    <div class="h-18 w-16 overflow-clip tabular-nums">
    +      <div class="flex flex-col">
    +        <For each={sequence(0, 10)}>
    +          {(tens) => (
    +            <div class="flex flex-row">
    +              <For each={sequence(0, 10)}>
    +                {(ones) => (
    +                  <div class="h-8 w-8">
    +                    {tens}
    +                    {ones}
    +                  </div>
    +                )}
    +              </For>
    +            </div>
    +          )}
    +        </For>
           </div>
         </div>
       );
    diff --git a/frontend/src/components/Stack.tsx b/frontend/src/components/Stack.tsx
    index 6e5f8e8..3f275a8 100644
    --- a/frontend/src/components/Stack.tsx
    +++ b/frontend/src/components/Stack.tsx
    @@ -1,26 +1,19 @@
     import clsx from "clsx";
    -import {
    -  children as resolve,
    -  Component,
    -  ComponentProps,
    -  Index,
    -  splitProps,
    -} from "solid-js";
    +import { Component, ComponentProps, splitProps } from "solid-js";
    
     export type StackProps = ComponentProps<"div">;
    
     export const Stack: Component<StackProps> = (props) => {
       const [local, rest] = splitProps(props, ["class", "children"]);
    -  const children = resolve(() => local.children);
       return (
         <div
           {...rest}
           class={clsx(
    -        "grid grid-cols-1 grid-rows-1 [&>*]:col-[1_/_1] [&>*]:row-[1_/_1]",
    +        "grid h-full w-full grid-cols-1 grid-rows-1 overflow-clip [&>*]:col-[1_/_1] [&>*]:row-[1_/_1]",
             local.class,
           )}
         >
    -      {children}
    +      {local.children}
         </div>
       );
     };

--- frontend/src/components/Picker.tsx
+++ frontend/src/components/Picker.tsx
@@ -1,20 +1,33 @@
 import clsx from "clsx";
-import { Component, createSignal } from "solid-js";
+import { Component, For, createSignal } from "solid-js";
+import { sequence } from "./utils";
+import { Stack } from "./Stack";
 
 export type PickerProps = {};
 
 export const Picker: Component<PickerProps> = (props) => {
   const [active, setActive] = createSignal(false);
+
+  const tensIndex = 0;
+  const onesIndex = 0;
+
   return (
-    <div>
-      <div
-        class={clsx(
-          "flex items-center justify-center bg-blue-50 transition-all",
-          active() ? "h-16 w-16" : "h-8 w-8",
-        )}
-        onClick={() => setActive((prev) => !prev)}
-      >
-        <div>42</div>
+    <div class="h-18 w-16 overflow-clip tabular-nums">
+      <div class="flex flex-col">
+        <For each={sequence(0, 10)}>
+          {(tens) => (
+            <div class="flex flex-row">
+              <For each={sequence(0, 10)}>
+                {(ones) => (
+                  <div class="h-8 w-8">
+                    {tens}
+                    {ones}
+                  </div>
+                )}
+              </For>
+            </div>
+          )}
+        </For>
       </div>
     </div>
   );
--- frontend/src/components/Stack.tsx
+++ frontend/src/components/Stack.tsx
@@ -1,26 +1,19 @@
 import clsx from "clsx";
-import {
-  children as resolve,
-  Component,
-  ComponentProps,
-  Index,
-  splitProps,
-} from "solid-js";
+import { Component, ComponentProps, splitProps } from "solid-js";
 
 export type StackProps = ComponentProps<"div">;
 
 export const Stack: Component<StackProps> = (props) => {
   const [local, rest] = splitProps(props, ["class", "children"]);
-  const children = resolve(() => local.children);
   return (
     <div
       {...rest}
       class={clsx(
-        "grid grid-cols-1 grid-rows-1 [&>*]:col-[1_/_1] [&>*]:row-[1_/_1]",
+        "grid h-full w-full grid-cols-1 grid-rows-1 overflow-clip [&>*]:col-[1_/_1] [&>*]:row-[1_/_1]",
         local.class,
       )}
     >
-      {children}
+      {local.children}
     </div>
   );
 };
